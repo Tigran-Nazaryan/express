8e0de846a1445ef0c39a3edca22ae37550501136
Tigran Nazaryan <tigran.nazaryan.18@gmail.com>
2025-08-27 15:29:24 +0400

    Include comments for each post with nested user (author) data

--- service/author.service.js
+++ service/author.service.js
@@ -1,8 +1,37 @@
-import { User, Post } from "../models/models.js";
+import {User, Post, Comment} from "../models/models.js";
 
 export const getAuthorWithPosts = async (userId) => {
     return await User.findByPk(userId, {
-        include: [{ model: Post, as: 'posts' }],
+        include: [
+            {
+                model: Post,
+                as: 'posts',
+                include: [
+                    {
+                        model: Comment,
+                        as: 'comments',
+                        include: [
+                            {
+                                model: User,
+                                as: 'user',
+                                attributes: ['id', 'firstName', 'lastName']
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                model: Comment,
+                as: 'comments',
+                include: [
+                    {
+                        model: Post,
+                        as: 'post',
+                        attributes: ['id', 'title']
+                    }
+                ]
+            }
+        ],
         attributes: {
             exclude: ["password"]
         }
